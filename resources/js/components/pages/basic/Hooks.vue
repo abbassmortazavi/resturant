12c75785b1a0841bd80785a57087a1b814103b7d
Programmer <abbassmortazavi@gmail.com>
2020-05-12 10:56:47 +0430

    hooks in vuejs

---
+++ resources/js/components/pages/basic/Hooks.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 ref="test">Hooks ...</h1>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    created(){
+        
+        
+    },
+    mounted(){
+        console.log(this.$refs.test);
+    }
+}
+</script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Panel from './components/pages/Panel';
+import Hooks from './components/pages/basic/Hooks';
 
 Vue.use(VueRouter);
 
@@ -8,11 +9,18 @@ const routes = [
     {
         path: '/panel',
         component: Panel
+    },
+
+    // vue hooks
+    {
+        path: '/hooks',
+        component: Hooks
     }
 ];
 
 const router = new VueRouter({
-    routes
+    routes,
+    mode: 'history'
 });
 
 export default router;
