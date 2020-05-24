2e1ee14e42c5c5fbe338137df7193f7056a822d7
Programmer <abbassmortazavi@gmail.com>
2020-05-24 20:47:24 +0430

    add store.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,15 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vuex from 'vuex';
 require('vue-multiselect/dist/vue-multiselect.min.css');
 import router from './router';
+
+//vuex
+Vue.use(Vuex);
+import storeData from './store';
+const store = new Vuex.Store(storeData);
+
 
 //View Ui
 import ViewUI from 'view-design';
@@ -23,5 +30,6 @@ Vue.component('main-app', require('./components/MainApp.vue').default);
 
 const app = new Vue({
     el: '#app',
-    router
+    router,
+    store
 });
